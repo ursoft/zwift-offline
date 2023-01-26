94eb36d6b16867e80d479979c6af3fda3e223691
ursoft <pershin@prosoftsystems.ru>
2023-01-26 15:09:00 +0500

    1st build with no issues (from docker)

--- scripts/ZwiftApp/build.cmd
+++ scripts/ZwiftApp/build.cmd
@@ -3,6 +3,5 @@ if "%1"=="Release" (
   set CFG=Release
 )
 git config --global --add safe.directory C:/zwiftapp_build/%CFG%/glfw-prefix/src/glfw
-rem git config --global --add safe.directory C:/zwiftapp_build/%CFG%/_deps/glfw-src
  
 cmake -B c:\ZwiftApp_build\%CFG% -S c:\ZwiftApp -DCMAKE_TOOLCHAIN_FILE=c:\vcpkg\scripts\buildsystems\vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static -DCMAKE_BUILD_TYPE=%CFG% -DBoost_NO_WARN_NEW_VERSIONS=1 -GNinja && cmake --build c:\ZwiftApp_build\%CFG%
