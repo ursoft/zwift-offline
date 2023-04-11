1b2e661dbfef8813077dc25664041c8764b7f262
ursoft <pershin@prosoftsystems.ru>
2023-04-12 00:11:34 +0500

    fixed the bug in stopwatch

--- scripts/ZwiftApp/cpp/Stopwatch.cpp
+++ scripts/ZwiftApp/cpp/Stopwatch.cpp
@@ -1,2 +1,9 @@
 #include "ZwiftApp.h"
 SteadyClock g_steadyClock;
+
+TEST(SmokeTest, Stopwatch100) {
+    Stopwatch w;
+    Sleep(100);
+    auto ms = w.elapsedInMilliseconds();
+    EXPECT_NEAR(110, ms, 10);
+}
--- scripts/ZwiftApp/cpp/Stopwatch.h
+++ scripts/ZwiftApp/cpp/Stopwatch.h
@@ -11,7 +11,7 @@ struct SteadyClock {
     uint64_t now() {
         LARGE_INTEGER PerformanceCount;
         QueryPerformanceCounter(&PerformanceCount);
-        uint64_t ret = (uint32_t)(int32_t)(PerformanceCount.LowPart * g_perfPeriod); //QUEST: why 32 bit
+        uint64_t ret = PerformanceCount.LowPart * g_perfPeriod; //QUEST: why 32 bit
         return ret;
     }
 };
@@ -24,8 +24,8 @@ struct Stopwatch {
             stop();
         return m_stop - m_start;
     }
-    uint64_t elapsedInMilliseconds() { return elapsed() / 1'000; }
-    uint64_t elapsedInSeconds() { return elapsed() / 1'000'000; }
+    uint64_t elapsedInMilliseconds() { return elapsed() / 1'000'000; }
+    uint64_t elapsedInSeconds() { return elapsed() / 1'000'000'000; }
     uint64_t partial() { return g_steadyClock.now() - m_start; }
     void start() { m_start = m_stop = g_steadyClock.now(); }
     void stop() { m_stop = g_steadyClock.now(); }
